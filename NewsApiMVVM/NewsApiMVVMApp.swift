1d351631e170e59f8cbf53bd620e1b3cb38e685f
Karlis <karlisstekels.dev@gmail.com>
2023-02-02 15:48:36 +0200

    Initial Commit

---
+++ NewsApiMVVM/NewsApiMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApiMVVMApp.swift
+//  NewsApiMVVM
+//
+//  Created by karlis.stekels on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApiMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
